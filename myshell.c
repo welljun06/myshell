f828533ef8cbbf4fb194dedef8ae7c683a9643bd
welljun06 <welljun06@gmail.com>
2018-05-19 23:45:38 +0800

    增加管道功能

--- myshell.c
+++ myshell.c
@@ -5,18 +5,27 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-char **get_input(char *);
+char **get_command(char *); // 分割“ ”
+char **get_pipe(char *); // 分割“|”
+char *trim_space(char *); // 去掉前后空格
+void run(char **, int , int );
 int cd(char *path);
 int main() {
     char **command;
     char *input;
     pid_t child_pid;
     int stat_loc;
+    int pipe_cnt;
 
     while (1) {
         input = readline("shell > ");
-        command = get_input(input);
-
+        command = get_pipe(input);
+        pipe_cnt = 0;
+        while(command[pipe_cnt]) {
+            printf("command: %s\n", command[pipe_cnt]);
+            pipe_cnt++;
+        }
+        printf("%d\n",pipe_cnt);
         if (!command[0]) {      // 处理输入为空
             free(input);
             free(command);
@@ -41,10 +50,11 @@ int main() {
             exit(1);
         }
         if (child_pid == 0) { // 子进程执行execvp
-            if (execvp(command[0], command) < 0) {
-                perror(command[0]);
-                exit(1);
-            }
+            // if (execvp(command[0], command) < 0) {
+            //     perror(command[0]);
+            //     exit(1);
+            // }
+            run(command, 0, pipe_cnt - 1);
         } else { // 父进程等待子进程结束
             waitpid(child_pid, &stat_loc, WUNTRACED);
         }
@@ -59,7 +69,7 @@ int main() {
 /**
  * 将用户输入按空格分解
  */
-char **get_input(char *input) {
+char **get_command(char *input) {
     char **command = malloc(8 * sizeof(char *));
     if (command == NULL) {
         perror("malloc failed");
@@ -83,6 +93,63 @@ char **get_input(char *input) {
     command[index] = NULL;
     return command;
 }
+char **get_pipe(char *input) {
+    char **command = malloc(8 * sizeof(char *));
+    if (command == NULL) {
+        perror("malloc failed");
+        exit(1);
+    }
+    char *separator = "|";
+    char *parsed;
+    int index = 0;
+    parsed = strtok(input, separator);
+    while (parsed != NULL) {
+        command[index] = trim_space(parsed);
+        index++;
+        parsed = strtok(NULL, separator);
+    }
+    command[index] = NULL;
+    return command;
+}
 int cd(char *path) {
     return chdir(path);
+}
+char *trim_space(char *in) {
+    int len = strlen(in);
+    int i = 0, j = len - 1;
+    while(in[i] == ' ' && in[i] != '\'') {
+        i++;
+    }
+    while(in[j] == ' ' && in[j] != '\'') {
+        in[j--] = 0;
+    }
+    return (in + i);
+}
+
+void run(char **command, int cur, int last) {
+    if(cur > last) {
+        exit(0);
+    }
+    char **args = get_command(command[cur]);
+
+    if(cur == last) {
+        execvp(args[0], args);
+    }
+    int fd[2], status;
+    pid_t pid;
+    pipe(fd);
+    if( (pid = fork()) == 0 ) {
+        dup2(fd[1], fileno(stdout));
+        close(fd[0]);
+        close(fd[1]);
+        if( (status = execvp(args[0], args) < 0 )) {
+            fprintf(stderr, "%s: command not found.\n",  args[0]);
+            exit(-1);
+        }
+    }
+
+    dup2(fd[0], fileno(stdin));
+    close(fd[0]);
+    close(fd[1]);
+    run(command, cur + 1, last);
 }
